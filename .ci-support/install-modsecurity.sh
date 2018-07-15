8a7259679b17be02e41588c8573a094256d8e5e5
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-07-14 22:00:19 -0300

    Add setup.py setuptools

---
+++ .ci-support/install-modsecurity.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get install -y flex byacc libpcre3 libpcre3-dev libxml2-dev git cmake libtool automake autoconf m4 pkg-config
+
+wget https://github.com/SpiderLabs/ModSecurity/releases/download/v3.0.2/modsecurity-v3.0.2.tar.gz
+
+tar xvf modsecurity-v3.0.2.tar.gz
+
+cd modsecurity-v3.0.2
+
+./build.sh
+./configure && make && make install
+
+cd ..
---
+++ .ci-support/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+
+apt-get install -y build-essential wget curl python3 python3-pip python3-dev
