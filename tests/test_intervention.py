fb1d5e8da7a7f333c4cf3ed1cf039cf5d286f455
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-09-14 19:05:26 -0300

    Add more tests

---
+++ tests/test_intervention.py
@@ -0,0 +1,18 @@
+import pytest
+import ModSecurity
+
+def test_intervention_processConnection(mocker, modsec, rules):
+    rule = 'SecRuleEngine On\n'
+    rule += 'SecRule REMOTE_ADDR "@ipMatch 127.0.0.1" "phase:0,deny,id:161"'
+
+    assert rules.load(rule) > 0, rules.getParserError() or 'Failed to load rule'
+
+    transaction = ModSecurity.Transaction(modsec, rules)
+    intervention = ModSecurity.ModSecurityIntervention()
+    assert not transaction.intervention(intervention)
+    assert not intervention.disruptive
+
+    transaction.processConnection('127.0.0.1', 33333, '127.0.0.1',
+                                  8080)
+    assert transaction.intervention(intervention)
+    assert intervention.disruptive
