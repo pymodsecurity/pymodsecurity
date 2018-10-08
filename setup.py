23fd480ce0263d283257704254625a77d3bc17c4
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-10-08 20:30:36 -0300

    [skip ci] Improve pypi classifiers

--- setup.py
+++ setup.py
@@ -111,7 +111,9 @@ setup(
     zip_safe=False,
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: POSIX :: Linux",
     ],
 )
