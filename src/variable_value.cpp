6a17a7e8ff2ddb62c23bfd64d47135be11be6120
Davysson Silva <mail@davysson.dev>
2020-04-29 13:56:02 -0300

    update VariableValue for 3.0.4

--- src/variable_value.cpp
+++ src/variable_value.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <modsecurity/variable_value.h>
 #include <modsecurity/variable_origin.h>
 #include <memory>
@@ -11,19 +12,15 @@ using modsecurity::VariableOrigin;
 void init_variable_value(py::module &m)
 {
     py::class_<VariableValue>(m, "VariableValue")
-        .def(py::init<>())
-        .def(py::init<const std::string *>())
-        .def(py::init<const std::string *, const std::string *>())
+        .def(py::init<const std::string *, const std::string *>(), py::arg("value") = nullptr)
         .def(py::init<const std::string *, const std::string *, const std::string *>())
         .def(py::init<const VariableValue *>())
-        .def_readwrite("m_key", &VariableValue::m_key)
-        .def_readwrite("m_value", &VariableValue::m_value)
-        .def_readwrite("m_col", &VariableValue::m_col)
-        .def_property("m_keyWithCollection", [](const VariableValue& v) {
-            return *v.m_keyWithCollection.get();
-        }, [](VariableValue& v, const std::string m_keyWithCollection) {
-            v.m_keyWithCollection = std::make_shared<std::string>(m_keyWithCollection);
-        }, py::return_value_policy::copy)
-        // .def_readwrite("m_orign", &VariableValue::m_orign)
+        .def("getKey", &VariableValue::getKey)
+        .def("getKeyWithCollection", &VariableValue::getKeyWithCollection)
+        .def("getCollection", &VariableValue::getCollection)
+        .def("getValue", &VariableValue::getValue)
+        .def("setValue", &VariableValue::setValue)
+        // .def("addOrigin", &Variable::addOrigin)
+        .def("getOrigin", &VariableValue::getOrigin)
         ;
 }
